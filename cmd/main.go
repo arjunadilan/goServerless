79aad361eeda2fcb0e29c0ab6812ade0ba72bb91
Dilan Arjuna <8019882+arjunadilan@users.noreply.github.com>
2022-04-29 08:22:52 +0800

    init

---
+++ cmd/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"github.com/arjunadilan/goServerlessAws/pkg/handlers"
+	"github.com/aws-sdk-go/aws"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"os"
+)
+
+const tableName = "LambdaInGoUser"
+
+func main() {
+	os.Setenv("AWS_ACCESS_KEY_ID", "")
+	os.Setenv("AWS_SECRET_ACCESS_KEY", "")
+	os.Setenv("AWS_REGION", "")
+
+	region := os.Getenv("AWS_REGION")
+	awsSession, err := session.NewSession(&aws.Config{
+		Region: aws.String(region),
+	})
+
+	if err != nil {
+		return
+	}
+
+	dynaClient := dynamodb.New(awsSession.New())
+	lambda.Start(handler)
+}
+
+func handler(req events.APIGatewayProxyRequest) (*events.APIGAtewayProxyResponse, error) {
+	switch req.HTTPMethod {
+	case "GET":
+		return handlers.GetUser(req, tableName, dynaClient)
+	case "POST":
+		return handlers.CreateUser(req, tableName, dynaClient)
+	case "PUT":
+	case
+	}
+}
